772757a8225b85d9105b47b6d7f2f6aba626abc3
fatmanmclone90 <fatmanmclone90@hotmail.com>
2024-04-22 10:57:03 +0100

    Update handlebar-functions.js
    Adds another sample function

--- build/functions/handlebar-functions.js
+++ build/functions/handlebar-functions.js
@@ -3,9 +3,16 @@
 // This functions swaps them to a local format e.g."August 09, 2021 - 08:31"
 /* Usage
     {{date_formatter_iso buildDetails.startTime}
+    {{jira_url (get_only_message_firstline this.message) }}
 */
 
 module.exports = {
     date_formatter_iso(theDate) {
-    return theDate.toISOString();
-}};
+        return theDate.toISOString();
+    },
+    jira_url(text) {
+        const regex = /([A-Z][A-Z0-9]{1,9}-[1-9][0-9]*)/gm;
+        const replaceUrl = "[$1](https://ezcorp.atlassian.net/browse/$1)"
+        return text.replace(regex, replaceUrl);
+    }
+};
